93308e31d570dc2e99ec24de93688b090de660a9
MoyeAnthonia <moye.anthonia@gmail.com>
2026-06-30 20:39:20 +1000

    Fix: the mediapipe source in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,20 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "strip-mediapipe-sourcemap-comments",
+      transform(code, id) {
+        if (id.includes("@mediapipe")) {
+          return {
+            code: code.replace(/\/\/# sourceMappingURL=\S+/g, ""),
+            map: null,
+          };
+        }
+      },
+    },
+  ],
   optimizeDeps: {
     exclude: ["@mediapipe/tasks-vision"],
   },
